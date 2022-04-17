859f3517fab23acc6148c1ff9401699cddaf249d
Nausheen Akhter <nausheen701@gmail.com>
2022-04-17 10:44:43 +0300

    search bar render

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,6 +15,21 @@ dynamicSearch = () => {
     return this.state.names.filter(name => name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
 }
 
+render(){
+    return(
+        <div>
+            <input
+                type= 'text' 
+                value = {this.state.searchTerm}
+                onChange = {this.editSearchTerm}
+                placeholder = 'Search for a species!'/>
+            <br></br>
+            <h3>Your search results</h3>
+            <SearchResultsContainer names = {this.dynamicSearch()}/>
+        </div>
+    )
+}
+
 }
 
 export default SearchBar
